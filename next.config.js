7df651461d188f43912fc6538e69e18e46841e15
howwieuwu <howwieuwu@gmail.com>
2024-06-20 11:23:24 -0500

    "arreglo configuraciones next"

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      domains: ["res.cloudinary.com", "https://asset.cloudinary.com"],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          port: "",
+          pathname: "/**",
+        },
+        {
+          protocol: "https",
+          hostname: "blog.logrocket.com",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
