a7687b6accd401e694ecab28163612e9c848998f
howwieuwu <howwieuwu@gmail.com>
2024-06-20 11:31:33 -0500

    "arreglo importacion"

--- src/app/dashboard/_components/Sidebar.jsx
+++ src/app/dashboard/_components/Sidebar.jsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useStore } from "@/context/store";
 import { ArrayElements } from "@/constants/ArrayElements";
-import { useState } from "react";
 
 export const Sidebar = () => {
   const saveAdmin = useStore((state) => state.sesionUser);
